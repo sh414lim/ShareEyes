432140a6829cde4085a5439f2b877fb8c87faa22
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-11-01 16:52:29 +0900

    .

--- src/page/Coins/coinsPrsenter.js
+++ src/page/Coins/coinsPrsenter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import Section from "../../Componets/Section";
+import Section from "../../Section/Coins";
 import Load from "../../Componets/Load";
 
 const Container=styled.div``;
--- src/page/Exchanges/ExchangesPresenter.js
+++ src/page/Exchanges/ExchangesPresenter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import Section from "../../Componets/Section";
+import Section from "../../Section/Exchanges";
 import Load from "../../Componets/Load";
 
 const Container=styled.div``;
