3c4475de98f138c588febc4fe467e297dffd419a
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-12-13 19:37:22 +0900

    setting

--- src/Componets/App.js
+++ src/Componets/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import GlobalStyles from "./Globalstyles";
 
 function App() {
   return (
     <div>
       <h1>Coin Explorer</h1>
-      <GlobalStyles />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Componets/App';
-import "./api";
 
 ReactDOM.render(
   <React.StrictMode>
